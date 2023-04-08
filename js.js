0b3dba05b7177a145f8a5c40c6eda4cef0d46e1d
*Ilya <fedaevi52@gmail.com>
2023-04-08 21:09:00 +0300

    create string

--- js.js
+++ js.js
@@ -8,4 +8,14 @@ let value;
 value = firstName + lastName;
 value = firstName + ' ' + lastName;
 value = value + ' I am' + age;
+
+value = firstName.length;
+value = firstName[2];
+value = firstName[4];
+value = lastName[lastName.length - 1];
+value = firstName[firstName.length - 1];
+
+
+value = firstName.toUpperCase();
+value = firstName.concat(' ', lastName);
 console.log(value);
