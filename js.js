3dfa400b69a00463358c0917253033e1889f13c5
*Ilya <fedaevi52@gmail.com>
2023-04-08 20:43:28 +0300

    course4

---
+++ js.js
@@ -0,0 +1,11 @@
+const firstName = 'Denis';
+const lastName = 'Kalinchacha';
+const age = 30;
+const str = 'Hello my name Denis';
+
+let value;
+
+value = firstName + lastName;
+value = firstName + ' ' + lastName;
+value = value + ' I am' + age;
+console.log(value);
